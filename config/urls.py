a0e9d49089ed0b5db52ad63d247c4e0dfd9f0930
sakshikumari431 <baliramsingh67@gmail.com>
2026-07-16 12:42:19 +0530

    home .html

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',views.home, name='home'),
+    path('',views.home, name='home'),
     path('', include('services.urls')),
 ]
